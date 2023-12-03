6ed7c31dd0662fa60f612aece1996d67cfb5c421
Anton <tepa-86@mail.ru>
2023-12-03 16:51:25 +0300

    Редактирование файла website_pars.py
    - добавление функции go_to_another_page (получение ссылки на другую страницу)
    - добавление функции getting_links_to_svg_files (получение ссылок на svg логотипы компаний, которые начинаются на "а")

--- website_pars.py
+++ website_pars.py
@@ -31,14 +31,46 @@ def get_links_to_categories(url: str) -> list:
     return category_links
 
 
-def main() -> None:
-    url = 'https://worldvectorlogo.com/alphabetical'
-    links_to_categories = get_links_to_categories(url)
+def go_to_another_page(url: str) -> str | None:
+    soup = fetch_html_content(url)
+    next_links = soup.find_all('a', class_='button fullwidth')
+
+    for next_btn in next_links:
+        if 'Next' in next_btn.get_text():
+            return next_btn.get('href')
+
+    return None
+
+
+def getting_links_to_svg_files(start_url: str) -> list:
+    url = start_url
+    svg_links = []
 
     count = 1
-    for link in links_to_categories:
-        print(f'{count}. {link}')
-        count += 1
+
+    while url:
+        soup = fetch_html_content(url)
+        company_logos = soup.find_all(class_='logo__container')
+
+        for logo in company_logos:
+            img_tag = logo.find('img')
+
+            if img_tag:
+                svg_links.append(img_tag['src'])
+                print(f"{count}. {img_tag['src']}")
+                count += 1
+
+        url = go_to_another_page(url)
+
+    return svg_links
+
+
+def main() -> None:
+    # url = 'https://worldvectorlogo.com/alphabetical'
+    # links_to_categories = get_links_to_categories(url)
+
+    url = 'https://worldvectorlogo.com/alphabetical/a'
+    getting_links_to_svg_files(url)
 
 
 if __name__ == '__main__':
