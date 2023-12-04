0ad3d2467c8db26f652e13fbce8a6a40a30d7b03
Anton <tepa-86@mail.ru>
2023-12-04 21:57:24 +0300

    Редактирование файла website_pars.py
    - добавление функции get_all_links_svg_files (получение всех ссылок на логотипы компаний по всем категориям)
    - добавление функции save_links_to_csv (сохранение собранных ссылок в CSV-файл)
    - добавление асинхронности

--- website_pars.py
+++ website_pars.py
@@ -1,20 +1,24 @@
-import requests
+import asyncio
+import aiohttp
+import pandas as pd
 from bs4 import BeautifulSoup
+from typing import List, Optional
 
 
-def fetch_html_content(url: str) -> BeautifulSoup | None:
+async def fetch_html_content(session: aiohttp.ClientSession, url: str) -> Optional[BeautifulSoup]:
     try:
-        response = requests.get(url)
+        async with session.get(url) as response:
 
-        if response.status_code == 200:
-            html_content = response.text
-            soup = BeautifulSoup(html_content, 'lxml')
+            if response.status == 200:
+                html_content = await response.text()
+                soup = BeautifulSoup(html_content, 'lxml')
 
-            return soup
-        else:
-            print(f"Ошибка: {response.status_code}")
+                return soup
 
-            return None
+            else:
+                print(f"Ошибка: {response.status}")
+
+                return None
 
     except Exception:
         print("Ошибка: Неправильно указан URL")
@@ -22,8 +26,8 @@ def fetch_html_content(url: str) -> BeautifulSoup | None:
         return None
 
 
-def get_links_to_categories(url: str) -> list:
-    soup = fetch_html_content(url)
+async def get_links_to_categories(session: aiohttp.ClientSession, url: str) -> List[str]:
+    soup = await fetch_html_content(session, url)
 
     categories = soup.find_all('a', class_='alphabet logo')
     category_links = [category['href'] for category in categories]
@@ -31,8 +35,8 @@ def get_links_to_categories(url: str) -> list:
     return category_links
 
 
-def go_to_another_page(url: str) -> str | None:
-    soup = fetch_html_content(url)
+async def go_to_another_page(session: aiohttp.ClientSession, url: str) -> Optional[str]:
+    soup = await fetch_html_content(session, url)
     next_links = soup.find_all('a', class_='button fullwidth')
 
     for next_btn in next_links:
@@ -42,14 +46,12 @@ def go_to_another_page(url: str) -> str | None:
     return None
 
 
-def getting_links_to_svg_files(start_url: str) -> list:
+async def get_links_to_svg_files_one_category(session: aiohttp.ClientSession, start_url: str) -> List[str]:
     url = start_url
     svg_links = []
 
-    count = 1
-
     while url:
-        soup = fetch_html_content(url)
+        soup = await fetch_html_content(session, url)
         company_logos = soup.find_all(class_='logo__container')
 
         for logo in company_logos:
@@ -57,21 +59,44 @@ def getting_links_to_svg_files(start_url: str) -> list:
 
             if img_tag:
                 svg_links.append(img_tag['src'])
-                print(f"{count}. {img_tag['src']}")
-                count += 1
 
-        url = go_to_another_page(url)
+        url = await go_to_another_page(session, url)
 
     return svg_links
 
 
-def main() -> None:
-    # url = 'https://worldvectorlogo.com/alphabetical'
-    # links_to_categories = get_links_to_categories(url)
+async def get_all_links_svg_files(session: aiohttp.ClientSession, url: str) -> List[List[str]]:
+    links_to_categories = await get_links_to_categories(session, url)
+    all_links_svg_files = []
 
-    url = 'https://worldvectorlogo.com/alphabetical/a'
-    getting_links_to_svg_files(url)
+    count = 1
+    for link in links_to_categories:
+        svg_file = await get_links_to_svg_files_one_category(session, link)
+
+        all_links_svg_files.append(svg_file)
+        print(f'{count} категория алфавита')
+        count += 1
+
+    return all_links_svg_files
+
+
+async def save_links_to_csv(links: list, path_csv: str) -> None:
+    flat_links = [link for sublist in links for link in sublist]
+    df = pd.DataFrame({"Ссылки на svg логотипы компаний": flat_links})
+    df.to_csv(path_csv, index=False, encoding='utf-8')
+
+    print(f"Ссылки сохранены в файл: {path_csv}")
+
+
+async def main() -> None:
+    url = 'https://worldvectorlogo.com/alphabetical'
+    path_csv_file = "links_to_svg_files.csv"
+
+    async with aiohttp.ClientSession() as session:
+        all_links_svg_files = await get_all_links_svg_files(session, url)
+
+        await save_links_to_csv(all_links_svg_files, path_csv_file)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
